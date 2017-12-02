e5413125bdfcd8674124284cc8434a29809dcee5
Enjila Mishra <enjila.mishra@ndus.edu>
2017-12-02 16:09:08 -0600

    first commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OHS.Startup))]
+namespace OHS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
